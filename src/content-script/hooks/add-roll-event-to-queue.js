481adcb8ff5bcc462cc74e9406f2bcb72c3e369e
Tobias Løfgren <tobloef@gmail.com>
2020-04-11 16:56:15 +0200

    Fix Roll20 variable name change

--- src/content-script/hooks/add-roll-event-to-queue.js
+++ src/content-script/hooks/add-roll-event-to-queue.js
@@ -3,11 +3,11 @@ import ScriptUrls from "../../shared/script-urls.js";
 const addRollEventToQueue = {
     name: "Add roll event to roll queue data",
     scriptUrls: [ScriptUrls.APP],
-    find: /S.push\({maxroll:n\[i\],callback:!1}\)/,
+    find: /S\.push\({maxroll:i\[n\],callback:!1}\)/,
     // language=JavaScript
     replaceWith: `
         S.push({
-            maxroll: n[i],
+            maxroll: i[n],
             callback: !1,
             rollEvent: e
         });
