fbb13a4926f0728f49e23fb0a56da4b73bffbd63
Tobias Løfgren <tobloef@gmail.com>
2020-12-29 11:29:30 +0100

    Format custom-dice-types.js

--- src/shared/custom-dice-types.js
+++ src/shared/custom-dice-types.js
@@ -1,109 +1,109 @@
 const CustomDiceTypes = {
-    ORIGINAL: {
-        key: "original",
-        name: "Original Roll20",
-        useColor: true,
-    },
-    INVERTED: {
-        key: "inverted",
-        name: "Colored Text (Black)",
-        useColor: true,
-    },
-    BORDER: {
-        key: "border",
-        name: "Bordered",
-        useColor: true,
-    },
-    BORDER_INVERTED: {
-        key: "border-inverted",
-        name: "Bordered (Inverted)",
-        useColor: true,
-    },
-    BLUE_HUE: {
-        key: "blue-hue",
-        name: "Bluehue",
-        useColor: false,
-    },
-    PURPLESTUFF: {
-        key: "purplestuff",
-        name: "Purple Stuff",
-        useColor: false,
-    },
-    FLAMEO: {
-        key: "flameo",
-        name: "Flameo",
-        useColor: false,
-    },
-    CYANICE: {
-        key: "cyanice",
-        name: "Cyanice",
-        useColor: false,
-    },
-    REGAL: {
-        key: "regal",
-        name: "Regal",
-        useColor: true,
-    },
-    FUNKY: {
-        key: "funky",
-        name: "Funky",
-        useColor: false,
-    },
-    FUCKYEAH: {
-        key: "fuckyeah",
-        name: "Fuck/Yes!",
-        useColor: true,
-    },
-    CHELLISH: {
-        key: "chellish",
-        name: "Chellish",
-        useColor: false,
-    },
-    PAINTERESQUE: {
-        key: "painteresque",
-        name: "Painteresque",
-        useColor: false,
-    },
-    FANCY: {
-        key: "fancy",
-        name: "Picture Test (d6 only)",
-        useColor: false,
-    },
-    RAVEN: {
-        key: "raven",
-        name: "Raven",
-        useColor: false,
-    },
-    MONK: {
-        key: "monk",
-        name: "Monk",
-        useColor: false,
-    },
-    TOME: {
+  ORIGINAL: {
+    key: "original",
+    name: "Original Roll20",
+    useColor: true,
+  },
+  INVERTED: {
+    key: "inverted",
+    name: "Colored Text (Black)",
+    useColor: true,
+  },
+  BORDER: {
+    key: "border",
+    name: "Bordered",
+    useColor: true,
+  },
+  BORDER_INVERTED: {
+    key: "border-inverted",
+    name: "Bordered (Inverted)",
+    useColor: true,
+  },
+  BLUE_HUE: {
+    key: "blue-hue",
+    name: "Bluehue",
+    useColor: false,
+  },
+  PURPLESTUFF: {
+    key: "purplestuff",
+    name: "Purple Stuff",
+    useColor: false,
+  },
+  FLAMEO: {
+    key: "flameo",
+    name: "Flameo",
+    useColor: false,
+  },
+  CYANICE: {
+    key: "cyanice",
+    name: "Cyanice",
+    useColor: false,
+  },
+  REGAL: {
+    key: "regal",
+    name: "Regal",
+    useColor: true,
+  },
+  FUNKY: {
+    key: "funky",
+    name: "Funky",
+    useColor: false,
+  },
+  FUCKYEAH: {
+    key: "fuckyeah",
+    name: "Fuck/Yes!",
+    useColor: true,
+  },
+  CHELLISH: {
+    key: "chellish",
+    name: "Chellish",
+    useColor: false,
+  },
+  PAINTERESQUE: {
+    key: "painteresque",
+    name: "Painteresque",
+    useColor: false,
+  },
+  FANCY: {
+    key: "fancy",
+    name: "Picture Test (d6 only)",
+    useColor: false,
+  },
+  RAVEN: {
+    key: "raven",
+    name: "Raven",
+    useColor: false,
+  },
+  MONK: {
+    key: "monk",
+    name: "Monk",
+    useColor: false,
+  },
+  TOME: {
     key: "tome",
     name: "Tome",
     useColor: false,
-    },
-    SUN: {
+  },
+  SUN: {
     key: "sun",
     name: "Sun",
     useColor: false,
-    },
-    ANCHOR: {
+  },
+  ANCHOR: {
     key: "anchor",
     name: "Anchor",
     useColor: false,
-    },
-    GREATAXE: {
+  },
+  GREATAXE: {
     key: "greataxe",
     name: "Greataxe",
     useColor: false,
-    },
-    Flower: {
+  },
+  Flower: {
     key: "flower",
     name: "Flower",
     useColor: false,
-    },
+  },
 };
 
 export default CustomDiceTypes;
